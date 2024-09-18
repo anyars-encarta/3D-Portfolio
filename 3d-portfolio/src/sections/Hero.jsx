f802a2b4b422ed3bccd019c810811cb0b2c43cbe
anyars-encarta <anyarsencarta@gmail.com>
2024-09-18 14:43:11 +0000

    Add a button to Hero section

--- 3d-portfolio/src/sections/Hero.jsx
+++ 3d-portfolio/src/sections/Hero.jsx
@@ -12,6 +12,7 @@ import ReactLogo from '../components/ReactLogo';
 import Cube from '../components/Cube';
 import Rings from '../components/Rings';
 import HeroCamera from '../components/HeroCamera';
+import Button from '../components/Button';
 
 const Hero = () => {
   // const x = useControls('HackerRoom', {
@@ -91,6 +92,12 @@ const Hero = () => {
             <directionalLight position={[10, 10, 10]} intensity={0.5} />
           </Suspense>
         </Canvas>
+      </div>
+
+      <div className='absolute bottom-7 left-0 right-0 w-full z-10 c-space'>
+        <a href="#contact" className='w-fit'>
+          <Button name="Let's work together" isBeam containerClass='sm:w-fit w-full sm:min-w-96' />
+        </a>
       </div>
     </section>
   )
