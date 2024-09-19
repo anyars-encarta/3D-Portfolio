a7ebcd19a552fd9bca1f1cfa5fbaee0651be2c0c
anyars-encarta <anyarsencarta@gmail.com>
2024-09-19 12:37:36 +0000

    Complete about section

--- 3d-portfolio/src/sections/About.jsx
+++ 3d-portfolio/src/sections/About.jsx
@@ -1,6 +1,25 @@
-import React from 'react'
+import Globe from 'react-globe.gl';
+import Button from '../components/Button';
+import { useState } from 'react';
 
 const About = () => {
+    const [copied, setCopied] = useState(false);
+
+    const handleCopy = () => {
+        if (navigator.clipboard) {
+          navigator.clipboard.writeText('anyarsencarta@gmail.com');
+          setCopied(true);
+      
+          setTimeout(() => {
+            setCopied(false)
+          }, 3000);
+        } else {
+          console.error("Clipboard API is not supported");
+        }
+      };
+
+    console.log(copied);
+
     return (
         <section className='c-space my-20' id='about'>
             <div className='grid xl:grid-cols-3 xl:grid-rows-6 md:grid-cols-2 grid-cols-1 gap-5 h-full'>
@@ -36,7 +55,61 @@ const About = () => {
                 <div className='col-span-1 xl:row-span-4'>
                     <div className='grid-container'>
                         <div className='rounded-3xl w-full sm:h-[326px] h-fit flex justify-center items-center'>
-                            
+                            <Globe
+                                height={326}
+                                width={326}
+                                backgroundColor='rgba(0, 0, 0, 0)'
+                                backgroundImageOpacity={0.5}
+                                showAtmosphere
+                                showGraticules
+                                globeImageUrl='//unpkg.com/three-globe/example/img/earth-night.jpg'
+                                bumpImageUrl='//unpkg.com/three-globe/example/img/earth-topology.png'
+                                labelsData={[{
+                                    lat: 6.6666, lng: 1.6163,
+                                    text: "I'm here",
+                                    color: 'white',
+                                    size: 20,
+                                }]}
+                            />
+                        </div>
+
+                        <div className=''>
+                            <p className='grid-headtext'>I work remotely accross most timezones</p>
+                            <p className='grid-subtext'>I am based in Kumasi, Ghana, West Africa, with remote work available</p>
+
+                            <Button 
+                                name = "Contact Me"
+                                isBeam
+                                containerClass='w-full mt-10'
+                            />
+                        </div>
+                    </div>
+                </div>
+
+                <div className='xl:col-span-2 xl:row-span-3'>
+                    <div className='grid-container'>
+                        <img src="/assets/grid3.png" alt="grid-3" className='w-full sm:h-[266px] h-fit object-contain' />
+
+                        <div>
+                            <p className='grid-headtext'>My passion for coding</p>
+                            <p className='grid-subtext'>I love solving problems, learning new technologies, 
+                                and building things through code. Coding isn't just my profession - it is my passion
+                            </p>
+                        </div>
+                    </div>
+                </div>
+
+                <div className='xl:col-span-1 xl:row-span-2'>
+                    <div className='grid-container'>
+                        <img src="/assets/grid4.png" alt="grid-4" className='w-full md:h-[126px] sm:h-[276px] h-fit object-cover sm:object-top' />
+
+                        <div className='space-y-2'>
+                            <p className='grid-subtext text-center'>Contact me</p>
+
+                            <div className='copy-container' onClick={handleCopy}>
+                                <img src={copied ? "/assets/check.svg" : "/assets/copy.svg"} alt="copy" />
+                                <p className='lg:text-2xl md:text-xl font-medium text-gray_gradient text-white'>anyarsencarta@gmail.com</p>
+                            </div>
                         </div>
                     </div>
                 </div>
