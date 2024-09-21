6e50eeffd111b8eac8b6e11b6cf07c8745dbb0fc
anyars-encarta <anyarsencarta@gmail.com>
2024-09-21 17:44:44 +0000

    Update projects

--- 3d-portfolio/src/constants/index.js
+++ 3d-portfolio/src/constants/index.js
@@ -294,14 +294,14 @@ export const navLinks = [
       ],
     },
     {
-      title: 'Imaginify - AI Photo Manipulation App',
-      desc: 'Imaginify is a groundbreaking Software-as-a-Service application that empowers users to create stunning photo manipulations using AI technology. With features like AI-driven image editing, a payments system, and a credits-based model.',
+      title: 'EncarToom - A Virtual Meeting App',
+      desc: 'EncarToom is a groundbreaking application that empowers users to schedule and hold virtual meetings. With features to share screens, record video, and add participants, it provides a seamless and productive way to engage with others.',
       subdesc:
-        'Built with Next.js 14, Cloudinary AI, Clerk, and Stripe, Imaginify combines cutting-edge technology with a user-centric approach. It can be turned into a side income or even a full-fledged business.',
-      href: 'https://www.youtube.com/watch?v=Ahwoks_dawU',
-      github: '',
+        'Built with Next.js 14, Clerk, and Radix UI, Stream-io, Encartoom combines cutting-edge technology with a user-centric approach. It can be used for personal or even industrial meetings and conferences, ensuring a seamless and productive business.',
+      href: 'https://encarta-zoom-clone.vercel.app/',
+      github: 'https://github.com/anyars-encarta/encarta-zoom-clone',
       texture: '/textures/project/project5.mp4',
-      logo: '/assets/project-logo5.png',
+      logo: '/assets/logo.svg',
       logoStyle: {
         backgroundColor: '#1C1A43',
         border: '0.2px solid #252262',
@@ -326,8 +326,193 @@ export const navLinks = [
         },
         {
           id: 4,
-          name: 'Framer Motion',
-          path: '/assets/framer.png',
+          name: 'Stream-io',
+          path: '/assets/streamio.png',
+        },
+      ],
+    },
+    {
+      title: 'Expenses Tracker - An Expenses Management App',
+      desc: 'Expenses Tracker is an innovative application that helps to manage income and expenses. With features to records incomes and expenses, it provides a seamless and productive way to track expenditures.',
+      subdesc:
+        'Built with React, Redux, Framer Motion, and Chakra UI, Expenses Tracker combines cutting-edge technology with a user-centric approach. It can be used to manage a wide variety of expenses, from personal expenses to business expenses.',
+      href: 'https://anyars-encarta.github.io/expense-tracker/',
+      github: 'https://github.com/anyars-encarta/expense-tracker',
+      texture: '/textures/project/project5.mp4',
+      logo: '/assets/expenses-tracker.png',
+      logoStyle: {
+        backgroundColor: '#1C1A43',
+        border: '0.2px solid #252262',
+        boxShadow: '0px 0px 60px 0px #635BFF4D',
+      },
+      spotlight: '/assets/spotlight2.png',
+      tags: [
+        {
+          id: 1,
+          name: 'React.js',
+          path: '/assets/react.svg',
+        },
+        {
+          id: 2,
+          name: 'TailwindCSS',
+          path: 'assets/tailwindcss.png',
+        },
+        {
+          id: 3,
+          name: 'TypeScript',
+          path: '/assets/typescript.png',
+        },
+        {
+          id: 4,
+          name: 'Stream-io',
+          path: '/assets/streamio.png',
+        },
+      ],
+    },
+    {
+      title: 'Chat App - An Interactive Chat App',
+      desc: 'Chat App is an innovative application that enhances communication and interaction between people. With features to share text, images, and files, it provides a seamless and productive way to communicate.',
+      subdesc:
+        'Built with React, Firebase, and Zustand, Chat App combines cutting-edge technology with a user-centric approach. It can be used to communicate and interact with a wide range of users.',
+      href: 'https://anyars-chat-app.vercel.app/',
+      github: 'https://github.com/anyars-encarta/encarta-chat-app',
+      texture: '/textures/project/project5.mp4',
+      logo: '/assets/chat-app.png',
+      logoStyle: {
+        backgroundColor: '#1C1A43',
+        border: '0.2px solid #252262',
+        boxShadow: '0px 0px 60px 0px #635BFF4D',
+      },
+      spotlight: '/assets/spotlight3.png',
+      tags: [
+        {
+          id: 1,
+          name: 'React.js',
+          path: '/assets/react.svg',
+        },
+        {
+          id: 2,
+          name: 'Google Firebase',
+          path: 'assets/firebase.png',
+        },
+        {
+          id: 3,
+          name: 'Zustand',
+          path: '/assets/zustand.png',
+        },
+      ],
+    },
+    {
+      title: 'Encarta Social - A Social Media Application',
+      desc: 'Encarta Social is an innovative social media application that enhances communication and interaction between people. With features to send and accept friendship requests, block and unblock users, share text, images, and files, it provides a seamless and productive way to socialize.',
+      subdesc:
+        'Built with React, Firebase, and Zustand, Chat App combines cutting-edge technology with a user-centric approach. It can be used to communicate and interact with a wide range of users.',
+      href: 'https://anyars-chat-app.vercel.app/',
+      github: 'https://github.com/anyars-encarta/encarta-chat-app',
+      texture: '/textures/project/project5.mp4',
+      logo: '/assets/social-media.png',
+      logoStyle: {
+        backgroundColor: '#1C1A43',
+        border: '0.2px solid #252262',
+        boxShadow: '0px 0px 60px 0px #635BFF4D',
+      },
+      spotlight: '/assets/spotlight4.png',
+      tags: [
+        {
+          id: 1,
+          name: 'React.js',
+          path: '/assets/react.svg',
+        },
+        {
+          id: 2,
+          name: 'Next JS',
+          path: 'assets/next.png',
+        },
+        {
+          id: 3,
+          name: 'MongoDB',
+          path: '/assets/mongodb.png',
+        },
+        {
+          id: 4,
+          name: 'Prisma',
+          path: '/assets/prisma.png',
+        },
+      ],
+    },
+    {
+      title: 'E-Montessori - A School Management System',
+      desc: 'E-Montessori is a groundbreaking school management system that enhances the mansgement of student and teachers schedules, lessons, and assignments. With features to schedule lessons for both students and teachers, manage student attendance, assignments and examinations, it provides a seamless and productive way to manage a school.',
+      subdesc:
+        'Built with React, Firebase, and Zustand, Chat App combines cutting-edge technology with a user-centric approach. It can be used to communicate and interact with a wide range of users.',
+      href: 'https://anyars-chat-app.vercel.app/',
+      github: 'https://github.com/anyars-encarta/encarta-chat-app',
+      texture: '/textures/project/project5.mp4',
+      logo: '/assets/school.png',
+      logoStyle: {
+        backgroundColor: '#1C1A43',
+        border: '0.2px solid #252262',
+        boxShadow: '0px 0px 60px 0px #635BFF4D',
+      },
+      spotlight: '/assets/spotlight4.png',
+      tags: [
+        {
+          id: 1,
+          name: 'React.js',
+          path: '/assets/react.svg',
+        },
+        {
+          id: 2,
+          name: 'Next JS',
+          path: 'assets/next.png',
+        },
+        {
+          id: 3,
+          name: 'Tailwind CSS',
+          path: '/assets/tailwindcss.png',
+        },
+        {
+          id: 4,
+          name: 'Recharts',
+          path: '/assets/recharts.png',
+        },
+      ],
+    },
+    {
+      title: 'MentorMe - A Mentorship / Counselling Application',
+      desc: 'MentorMe is an innovative mentorship or counselling application that enhances communication and interaction between people. With features to book appointments, hold virtual counselling and mentorship sessions, it provides a seamless and productive way to hold meetings and connect with a wide range of users.',
+      subdesc:
+        'Built with React, Firebase, and Zustand, Chat App combines cutting-edge technology with a user-centric approach. It can be used to communicate and interact with a wide range of users.',
+      href: 'https://anyars-chat-app.vercel.app/',
+      github: 'https://github.com/anyars-encarta/encarta-chat-app',
+      texture: '/textures/project/project5.mp4',
+      logo: '/assets/mentor-me-icon.png',
+      logoStyle: {
+        backgroundColor: '#1C1A43',
+        border: '0.2px solid #252262',
+        boxShadow: '0px 0px 60px 0px #635BFF4D',
+      },
+      spotlight: '/assets/spotlight4.png',
+      tags: [
+        {
+          id: 1,
+          name: 'React.js',
+          path: '/assets/react.svg',
+        },
+        {
+          id: 2,
+          name: 'Next JS',
+          path: 'assets/next.png',
+        },
+        {
+          id: 3,
+          name: 'Stream-io',
+          path: '/assets/streamio.png',
+        },
+        {
+          id: 4,
+          name: 'TypeScript',
+          path: '/assets/typescript.png',
         },
       ],
     },
