7d1ba61dbac8d2b5049116a4a46f2565cd3c5333
anyars-encarta <anyarsencarta@gmail.com>
2024-09-23 10:45:16 +0000

    Simplify handleScroll function

--- 3d-portfolio/src/components/NavItems.jsx
+++ 3d-portfolio/src/components/NavItems.jsx
@@ -2,14 +2,15 @@ import React from 'react'
 import { navLinks } from '../constants'
 
 const NavItems = ({ toggleMenu }) => {
+    const handleScroll = () => {
+        window.scrollTo(0, 0);
+        toggleMenu();
+    }
     return (
         <ul className='nav-ul'>
             {navLinks.map(({ id, name, href }) => (
                 <li key={id} className='nav-li'>
-                    <a href={href} className='nav-li_a' onClick={() => {
-                        window.scrollTo(0, 0);
-                        toggleMenu();
-                    }}>
+                    <a href={href} className='nav-li_a' onClick={handleScroll}>
                         {name}
                     </a>
                 </li>
