3e321c4864685d043605dda081c9b46405862150
anyars-encarta <anyarsencarta@gmail.com>
2024-09-23 10:42:16 +0000

    Close Menu when clicked outside of it

--- 3d-portfolio/src/sections/Navbar.jsx
+++ 3d-portfolio/src/sections/Navbar.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import NavItems from "../components/NavItems";
 
 const Navbar = () => {
@@ -8,8 +8,22 @@ const Navbar = () => {
         setIsOpen((prev) => !prev);
     };
 
+    useEffect(() => {
+        const handleDocumentClick = (event) => {
+          if (!event.target.closest('.nav-main')) {
+            toggleMenu();
+          }
+        };
+    
+        document.addEventListener('click', handleDocumentClick);
+    
+        return () => {
+          document.removeEventListener('click', handleDocumentClick);
+        };
+      }, [toggleMenu]);
+
     return (
-        <header className='fixed top-0 left-0 right-0 z-50 bg-black/90'>
+        <header className='nav-main fixed top-0 left-0 right-0 z-50 bg-black/90'>
             <div className='max-w-7xl mx-auto'>
                 <div className='flex justify-between items-center py-5 mx-auto c-space'>
                     <a href="/" className="text-neutral-400 font-bold text-xl hover:text-white transition-colors">
