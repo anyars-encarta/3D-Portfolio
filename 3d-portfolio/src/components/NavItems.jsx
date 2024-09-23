611aa4b5bcf6b9e26d6f5855253cab4e462699b8
anyars-encarta <anyarsencarta@gmail.com>
2024-09-23 10:57:35 +0000

    Correct nav menu selection

--- 3d-portfolio/src/components/NavItems.jsx
+++ 3d-portfolio/src/components/NavItems.jsx
@@ -1,16 +1,16 @@
-import React from 'react'
 import { navLinks } from '../constants'
 
 const NavItems = ({ toggleMenu }) => {
-    const handleScroll = () => {
-        window.scrollTo(0, 0);
+    const handleScroll = (href) => {
+        window.scrollTo(0, document.querySelector(href).offsetTop);
         toggleMenu();
-    }
+    };
+    
     return (
         <ul className='nav-ul'>
             {navLinks.map(({ id, name, href }) => (
-                <li key={id} className='nav-li'>
-                    <a href={href} className='nav-li_a' onClick={handleScroll}>
+                <li key={id} className='nav-li' onClick={() => handleScroll(href)}>
+                    <a href={href} className='nav-li_a'>
                         {name}
                     </a>
                 </li>
