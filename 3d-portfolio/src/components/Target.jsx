bb1c7e26bc154d26494a5215ed7210bdebf5bf55
Anyars Yussif <anyarsencarta@gmail.com>
2025-11-28 13:24:46 +0000

    Correct 3d model display on Hero section

--- 3d-portfolio/src/components/Target.jsx
+++ 3d-portfolio/src/components/Target.jsx
@@ -5,7 +5,7 @@ import { useGSAP } from '@gsap/react';
 
 const Target = (props) => {
     const targetRef = useRef();
-    const { scene } = useGLTF('https://vazxmixjsiawhamofees.supabase.co/storage/v1/object/public/models/target-stand/model.gltf')
+    const { scene } = useGLTF('/models/computer-desk-area/scene.gltf')
     
     useGSAP(() => {
         gsap.to(targetRef.current.position, {
